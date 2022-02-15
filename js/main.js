3b34d01477586f1cebb92af6b8fb35033f4baa51
Mahmoud Alhato <mahmoudalhato2@gmail.com>
2022-02-15 23:42:53 +0200

    add action on icons in the header Relates #11

--- js/main.js
+++ js/main.js
@@ -4,8 +4,16 @@ window.onscroll = (() => {
     if (window.pageYOffset > sticky) {
         header.style.transition = "all 1s";
         header.classList.add("sticky");
-
+        faceBtn.style.transition = "all 1s";
+        faceBtn.classList.add("face-ch");
+        instaBtn.style.transition = "all 1s";
+        instaBtn.classList.add("face-ch");
+        twitterBtn.style.transition = "all 1s";
+        twitterBtn.classList.add("face-ch");
     } else {
         header.classList.remove("sticky");
+        faceBtn.classList.remove("face-ch");
+        instaBtn.classList.remove("face-ch");
+        twitterBtn.classList.remove("face-ch");
     }
 })
