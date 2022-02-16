a43fb0454b3f9eee4bf32c270a1e8961e7a9f5e0
Mahmoud Alhato <mahmoudalhato2@gmail.com>
2022-02-16 15:37:18 +0200

    fixing bug Relates #16

--- js/api.js
+++ js/api.js
@@ -17,6 +17,7 @@ const req = ((method, url, cb) => {
 
 const changeImg = ((data) => {
     if (data.media_type === "image") {
+        console.log(data.hdurl);
         mainImg.src = data.hdurl;
     } else {
         req("GET", `https://api.nasa.gov/planetary/apod?api_key=oSnqH9JmVcGOO58lqnKyVzgeWfd0VvokmLCLYFWX&date=${year}-${month}-${day-3}`, changeImg);
@@ -26,6 +27,4 @@ req("GET", `https://api.nasa.gov/planetary/apod?api_key=fth4fhaP5VEF5XBRzERX5xUb
 
 
 
-req('GET' , `https://pixabay.com/api/?key=24477339-22b8ee8a367b2cdaebd16eb69&q=${value}` , LoopImages);
-
-
+req('GET', `https://pixabay.com/api/?key=24477339-22b8ee8a367b2cdaebd16eb69&q=${value}`, LoopImages);
