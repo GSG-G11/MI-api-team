3fae7ea42ae01f592ae8191f9352fba3cf0499ef
Mahmoud Alhato <mahmoudalhato2@gmail.com>
2022-02-16 19:17:25 +0200

    added extra things in all api's Relates #24

--- js/dom.js
+++ js/dom.js
@@ -4,9 +4,24 @@ const faceBtn = document.querySelector("#face-btn");
 const instaBtn = document.querySelector("#insta-btn");
 const twitterBtn = document.querySelector("#twitter-btn");
 const imgAndVideoDiv = document.querySelector("#main-img-div");
-
+const galaxyBtn = document.querySelector("#galaxy");
+const lifeStyleBtn = document.querySelector("#LifeStyle");
+const foodBtn = document.querySelector("#Food");
+const weddingBtn = document.querySelector("#Wedding");
 const imageContainer = document.querySelector("#imgs-container");
+const resultImgOrVideos = document.querySelector("#description-result");
 
+
+const creatTitleAndDescription = ((data) => {
+    const imgOrVideoTitle = document.createElement("h4");
+    imgOrVideoTitle.setAttribute("id", "title-of-img-or-video");
+    resultImgOrVideos.appendChild(imgOrVideoTitle);
+    const description = document.createElement("p");
+    description.setAttribute("id", "description-of-img-or-video");
+    resultImgOrVideos.appendChild(description);
+    imgOrVideoTitle.textContent = data.title;
+    description.textContent = data.explanation;
+})
 const createBoxImg = (data) => {
     const createImgBox = document.createElement("div");
     createImgBox.classList.add("imgs-box");
@@ -38,8 +53,13 @@ const LoopImages = (data) => {
 
 const videoAndImg = ((data) => {
     if (data.media_type === "image") {
-        mainImg.src = data.hdurl;
+        creatTitleAndDescription(data);
+        const imgMain = document.createElement("img");
+        imgMain.setAttribute("id", "main-img");
+        imgAndVideoDiv.appendChild(imgMain);
+        imgMain.src = data.hdurl;
     } else {
+        creatTitleAndDescription(data);
         mainImg.style.display = "none";
         const vid = document.createElement("iframe");
         vid.setAttribute("id", "videos-style")
@@ -47,3 +67,33 @@ const videoAndImg = ((data) => {
         vid.src = data.url;
     }
 })
+
+const removeAllElements = (() => {
+    while (imageContainer.firstChild) {
+        imageContainer.removeChild(imageContainer.lastChild);
+    }
+})
+
+galaxyBtn.addEventListener("click", (() => {
+    removeAllElements();
+    value = "galaxy";
+    req('GET', `https://pixabay.com/api/?key=24477339-22b8ee8a367b2cdaebd16eb69&q=${value}`, bringImg);
+}))
+
+lifeStyleBtn.addEventListener("click", (() => {
+    removeAllElements();
+    value = "LifeStyle";
+    req('GET', `https://pixabay.com/api/?key=24477339-22b8ee8a367b2cdaebd16eb69&q=${value}`, bringImg);
+}))
+
+foodBtn.addEventListener("click", (() => {
+    removeAllElements();
+    value = "food";
+    req('GET', `https://pixabay.com/api/?key=24477339-22b8ee8a367b2cdaebd16eb69&q=${value}`, bringImg);
+}))
+
+weddingBtn.addEventListener("click", (() => {
+    removeAllElements();
+    value = "wedding";
+    req('GET', `https://pixabay.com/api/?key=24477339-22b8ee8a367b2cdaebd16eb69&q=${value}`, bringImg);
+}))
