5bf7a71b7b5436830176282cd5afc19e94682cfb
Mahmoud Alhato <mahmoudalhato2@gmail.com>
2022-02-16 14:06:17 +0200

    add responsive to all page except the footer Relates #13

--- js/api.js
+++ js/api.js
@@ -2,7 +2,6 @@ let time = new Date();
 let year = time.getFullYear();
 let month = time.getMonth() + 1;
 let day = time.getDate();
-
 const req = ((method, url, cb) => {
     let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
@@ -15,6 +14,10 @@ const req = ((method, url, cb) => {
 })
 
 const changeImg = ((data) => {
-    mainImg.src = data.hdurl;
+    if (data.media_type === "image") {
+        mainImg.src = data.hdurl;
+    } else {
+        req("GET", `https://api.nasa.gov/planetary/apod?api_key=oSnqH9JmVcGOO58lqnKyVzgeWfd0VvokmLCLYFWX&date=${year}-${month}-${day-3}`, changeImg);
+    }
 })
-req("GET", `https://api.nasa.gov/planetary/apod?api_key=fth4fhaP5VEF5XBRzERX5xUb7TPCj3ERo2hmC3zO&date=${year}-${month}-${day}`, changeImg);
+req("GET", `https://api.nasa.gov/planetary/apod?api_key=oSnqH9JmVcGOO58lqnKyVzgeWfd0VvokmLCLYFWX&date=${year}-${month}-${day}`, changeImg);
